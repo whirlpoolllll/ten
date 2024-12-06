a659113a2f55ab0787b117a7f8c6da53a62dd79f
The Storm b4 The Calm <56097629+whirlpoolllll@users.noreply.github.com>
2024-12-06 17:56:03 -0500

    Add files via upload

---
+++ docs/generate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DIRECTORY=$(dirname -- "$0")
+
+cd "$DIRECTORY"
+
+rm -rf ./pdoc
+pdoc3 --html -c sort_identifiers=False --force ../tenlib --output-dir . --skip-errors
+mkdocs build
+
+echo
+echo
+echo ---------------------------------
+echo Documentation is available at: $PWD/site/index.html
+echo ---------------------------------
